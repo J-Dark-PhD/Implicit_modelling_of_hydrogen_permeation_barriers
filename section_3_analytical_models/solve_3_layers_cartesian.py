2bb029ec3a3341d8372c07d8dda16173b6812313
J Dark <james.dark.95@gmail.com>
2024-07-25 09:47:01 +0200

    updated with print statements

--- section_3_analytical_models/solve_3_layers_cartesian.py
+++ section_3_analytical_models/solve_3_layers_cartesian.py
@@ -3,7 +3,7 @@ from sympy import *
 D1, D2 = symbols("D1, D2")
 S1, S2 = symbols("S1, S2")
 e, L = symbols("e, L")
-c_0, c_L = symbols("c_0, c_L")
+c_0, c_1 = symbols("c_0, c_1")
 a1, a2, a3, b1, b2, b3 = symbols("a1, a2, a3, b1, b2, b3")
 x_0 = 0
 x = Symbol("x")
@@ -19,20 +19,27 @@ list_of_equations = [
     D1 * diff(u1, x).subs(x, e) - D2 * diff(u2, x).subs(x, e),
     D2 * diff(u2, x).subs(x, e + L) - D1 * diff(u3, x).subs(x, e + L),
     u1.subs(x, x_0) - c_0,
-    u3.subs(x, L + (2 * e)) - c_L,
+    u3.subs(x, L + (2 * e)) - c_1,
 ]
 
 
 res = solve(list_of_equations, a1, a2, a3, b1, b2, b3)
 
+print(f"a1 = {latex(res[a1])}")
+print(f"a2 = {latex(res[a2])}")
+print(f"a3 = {latex(res[a3])}")
+print(f"b1 = {latex(res[b1])}")
+print(f"b2 = {latex(res[b2])}")
+print(f"b3 = {latex(res[b3])}")
 
-def compute_flux(e, D1, D2, S1, S2, L, pressure):
+
+def compute_flux(e, D1, D2, S1, S2, L, c_0, c_1):
     new_a = res[a2]
     new_a = new_a.subs("e", e)
     new_a = new_a.subs("D1", D1)
     new_a = new_a.subs("S1", S1)
-    new_a = new_a.subs("c_0", pressure**0.5 * S1)
-    new_a = new_a.subs("c_L", 0)
+    new_a = new_a.subs("c_0", c_0)
+    new_a = new_a.subs("c_1", c_1)
     new_a = new_a.subs("L", L)
     new_a = new_a.subs("D2", D2)
     new_a = new_a.subs("S2", S2)
@@ -42,8 +49,11 @@ def compute_flux(e, D1, D2, S1, S2, L, pressure):
 
 
 def compute_PRF(e, D1, D2, S1, S2, L, pressure):
-    flux_with_barrier = compute_flux(e, D1, D2, S1, S2, L, pressure)
-    flux_wo_barrier = compute_flux(0, D2, D2, S2, S2, L, pressure)
+    flux_with_barrier = compute_flux(e, D1, D2, S1, S2, L, pressure**0.5 * S1, 0)
+    print(f"flux with barrier = {latex(flux_with_barrier)}")
+
+    flux_wo_barrier = compute_flux(0, D2, D2, S2, S2, L, pressure**0.5 * S2, 0)
+    print(f"flux without barrier = {latex(flux_wo_barrier)}")
 
     return flux_wo_barrier / flux_with_barrier
 
@@ -53,4 +63,4 @@ PRF_expression = simplify(
 )
 
 if __name__ == "__main__":
-    print(latex(PRF_expression))
+    print(f"PRF = {latex(PRF_expression)}")
--- section_3_analytical_models/solve_3_layers_cylindrical.py
+++ section_3_analytical_models/solve_3_layers_cylindrical.py
@@ -9,7 +9,7 @@ D1, D2 = symbols("D1, D2")
 S1, S2 = symbols("S1, S2")
 e, L = symbols("e L")
 r, r_0 = symbols("r r_0")
-u_0, u_L = symbols("c_0, c_L")
+c_0, c_1 = symbols("c_0, c_1")
 a1, a2, a3, b1, b2, b3 = symbols("a1, a2, a3, b1, b2, b3")
 
 u1 = a1 * log(r) + b1
@@ -23,22 +23,29 @@ list_of_equations = [
     u2.subs(r, r_interface_2) / S2 - u3.subs(r, r_interface_2) / S1,
     D1 * grad(u1).subs(r, r_interface_1) - D2 * grad(u2).subs(r, r_interface_1),
     D2 * grad(u2).subs(r, r_interface_2) - D1 * grad(u3).subs(r, r_interface_2),
-    u1.subs(r, r_0) - u_0,
-    u3.subs(r, r_0 + L + 2 * e) - u_L,
+    u1.subs(r, r_0) - c_0,
+    u3.subs(r, r_0 + L + 2 * e) - c_1,
 ]
 
 # for some reason takes a bit of time...
 res = solve(list_of_equations, a1, a2, a3, b1, b2, b3)
 
+print(f"a1 = {latex(res[a1])}")
+print(f"a2 = {latex(res[a2])}")
+print(f"a3 = {latex(res[a3])}")
+print(f"b1 = {latex(res[b1])}")
+print(f"b2 = {latex(res[b2])}")
+print(f"b3 = {latex(res[b3])}")
 
-def compute_flux(e, r_0, D1, D2, S1, S2, L, c_0):
+
+def compute_flux(e, r_0, D1, D2, S1, S2, L, c_0, c_1):
     new_a = res[a2]
     new_a = new_a.subs("e", e)
     new_a = new_a.subs("r_0", r_0)
     new_a = new_a.subs("D1", D1)
     new_a = new_a.subs("S1", S1)
     new_a = new_a.subs("c_0", c_0)
-    new_a = new_a.subs("c_L", 0)
+    new_a = new_a.subs("c_1", c_1)
     new_a = new_a.subs("L", L)
     new_a = new_a.subs("D2", D2)
     new_a = new_a.subs("S2", S2)
@@ -47,8 +54,13 @@ def compute_flux(e, r_0, D1, D2, S1, S2, L, c_0):
 
 
 def compute_PRF(e, r_0, D1, D2, S1, S2, L, pressure):
-    flux_with_barrier = compute_flux(e, r_0 - e, D1, D2, S1, S2, L, pressure**0.5 * S1)
-    flux_wo_barrier = compute_flux(0, r_0, D2, D2, S2, S2, L, pressure**0.5 * S2)
+    flux_with_barrier = compute_flux(
+        e, r_0 - e, D1, D2, S1, S2, L, pressure**0.5 * S1, 0
+    )
+    print(f"flux with barrier = {latex(flux_with_barrier)}")
+
+    flux_wo_barrier = compute_flux(0, r_0, D2, D2, S2, S2, L, pressure**0.5 * S2, 0)
+    print(f"flux without barrier = {latex(flux_wo_barrier)}")
 
     return flux_wo_barrier / flux_with_barrier
 
